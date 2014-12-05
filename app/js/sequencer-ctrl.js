1903aef2c4d5b2877339bf7bdf4b07896c23d5f2
Zach <briggszj@gmail.com>
2014-12-05 17:10:11 -0600

    Play the instruments

--- app/js/sequencer-ctrl.js
+++ app/js/sequencer-ctrl.js
@@ -6,7 +6,8 @@
     // ======== STATE ========
     var patterns = {
       CYmbal: [],
-      HiHat: [3, 7, 11, 15],
+      ClosedHihat: [3, 7, 11, 15],
+      OpenHihat: [],
       HCP: [],
       COWbell: [],
       HiTom: [],
@@ -21,12 +22,27 @@
 
     $scope.currentStep = 0;
 
-    $scope.bpm = 60;
+    $scope.bpm = 128;
     // =======================
 
 
     // 8 steps = 1 bar = 4 beats
     $scope.steps = _.range(1, 17);
+
+    var stepInstruments = $scope.steps.map(function(step) {
+      return {
+        CYmbal: new Audio('/Cymbal/Cym80815.wav'),
+        HCP: new Audio('/Clap/Clap80823.wav'),
+        COWbell: new Audio('/CowBell/Cowbell80810.wav'),
+        HiTom: new Audio('/Toms/Tom80844.wav'),
+        MidTom: new Audio('/Toms/Tom80819.wav'),
+        LowTom: new Audio('/Toms/Tom8086.wav'),
+        ClosedHihat: new Audio('/ClosedHiHat/CH80833.wav'),
+        OpenHihat: new Audio('/OpenHiHat/OH80817.wav'),
+        SnareDrum: new Audio('/SnareDrum/Snare80837.wav'),
+        BassDrum: new Audio('/BassDrum/Un-Pitched/Kick80810.wav'),
+      };
+    });
 
 
     $scope.isRunning = function () {
@@ -54,7 +70,7 @@
     };
 
     function msToNextStep(bpm) {
-      return (((60 / bpm) * 4) / 8) * 1000;
+      return ((60 / bpm)  / 4) * 1000;
     }
 
     function tick() {
@@ -62,6 +78,11 @@
 
       return $timeout(function () {
         $scope.currentStep = ($scope.currentStep + 1) % 16 || 16;
+        _(stepInstruments[$scope.currentStep - 1]).each(function(instrument, key) {
+          if (patterns[key].indexOf($scope.currentStep) > -1) {
+            instrument.play()
+          }
+        });
         tickId = tick();
       }, msToNextStep(bpm));
     }
