b9767f3f4f4956b62d66840489f96bf985b03412
Zach <briggszj@gmail.com>
2014-12-12 10:19:29 -0600

    Remove unused $scope attributes.

--- app/js/sequencer-ctrl.js
+++ app/js/sequencer-ctrl.js
@@ -6,41 +6,37 @@
     function makeTracks() {
       return {
         CYmbal: { url: '/Cymbal/Cym80815.wav', pattern: {} },
+        ClosedHihat: { url: '/ClosedHiHat/CH80833.wav', pattern: {} },
+        OpenHihat: { url: '/OpenHiHat/OH80817.wav', pattern: {} },
         HCP: { url: '/Clap/Clap80823.wav', pattern: {} },
         COWbell: { url: '/CowBell/Cowbell80810.wav', pattern: {} },
         HiTom: { url: '/Toms/Tom80844.wav', pattern: {} },
         MidTom: { url: '/Toms/Tom80819.wav', pattern: {} },
         LowTom: { url: '/Toms/Tom8086.wav', pattern: {} },
-        RimShot: { url: '/RimShot/Rim8088.wav', pattern: {} },
-        CLaves: { url: '/Claves/Clave8084.wav', pattern: {} },
-        ClosedHihat: { url: '/ClosedHiHat/CH80833.wav', pattern: {} },
-        OpenHihat: { url: '/OpenHiHat/OH80817.wav', pattern: {} },
         SnareDrum: { url: '/SnareDrum/Snare80837.wav', pattern: {} },
         BassDrum: { url: '/BassDrum/Un-Pitched/Kick80810.wav', pattern: {} },
+        RimShot: { url: '/RimShot/Rim8088.wav', pattern: {} },
+        CLaves: { url: '/Claves/Clave8084.wav', pattern: {} },
       };
     }
 
     // ======== STATE ========
 
-    $scope.variations = [{
-      tracks: makeTracks(),
-      duration: 16,
-    }, {
+    var tickId = null;
+    var variations = [{
       tracks: makeTracks(),
       duration: 8,
     }, {
       tracks: makeTracks(),
+      duration: 16,
+    }, {
+      tracks: makeTracks(),
       duration: 32,
     }];
-
-    var tickId = null;
-
-    $scope.currentStep = 0;
+    var currentStep = 0;
 
     $scope.bpm = 128;
-
     $scope.duration = 8;
-
     $scope.steps = [];
 
     // =======================
@@ -53,10 +49,10 @@
 
     function tick() {
       return $timeout(function () {
-        $scope.currentStep += 1;
+        currentStep += 1;
 
-        $scope.variations.forEach(function (variation) {
-          var step = $scope.currentStep % variation.duration || variation.duration;
+        variations.forEach(function (variation) {
+          var step = currentStep % variation.duration || variation.duration;
           _(variation.tracks).forEach(function (track) {
             if (track.pattern[step] !== undefined) {
               track.pattern[step].play();
@@ -76,13 +72,12 @@
       $scope.bpm = parseInt(bpm, 10);
     };
 
-    $scope.setDuration = function(duration) {
-      $scope.duration = parseInt(duration, 10);
-      $scope.steps =  _.range(1, parseInt(duration, 10) + 1);
+    $scope.setDuration = function() {
+      $scope.steps =  _.range(1, $scope.duration + 1);
     };
 
     $scope.toggleStep = function (instrument, step) {
-      var track = _($scope.variations).findWhere({ duration: $scope.duration }).tracks[instrument];
+      var track = _(variations).findWhere({ duration: $scope.duration }).tracks[instrument];
 
       if (track.pattern[step] === undefined) {
         track.pattern[step] = new Audio(track.url);
@@ -92,7 +87,7 @@
     };
 
     $scope.note = function (instrument, step) {
-      var track = _($scope.variations).findWhere({ duration: $scope.duration }).tracks[instrument];
+      var track = _(variations).findWhere({ duration: $scope.duration }).tracks[instrument];
 
       if (track.pattern[step] === undefined) {
         return "";
@@ -104,7 +99,7 @@
     $scope.toggleSequence = function () {
       if ($scope.isRunning()) {
         $timeout.cancel(tickId);
-        $scope.currentStep = 0;
+        currentStep = 0;
         tickId = null;
       } else {
         tickId = tick();
@@ -112,10 +107,10 @@
     };
 
     $scope.isCurrentStep = function(step) {
-      if ($scope.currentStep === 0) {
+      if (currentStep === 0) {
         return false;
       } else {
-        return ($scope.currentStep % $scope.duration || $scope.duration) === step;
+        return (currentStep % $scope.duration || $scope.duration) === step;
       }
     };
 
